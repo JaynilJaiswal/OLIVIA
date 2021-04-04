2f0e9aee6c2351e75e993ab04f109f0c175db0d6
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2021-04-04 19:57:59 +0530

    minor fle

---
+++ findContactInfo.py
@@ -0,0 +1,22 @@
+from models.user import db, User,User_location ,User_command_history, User_music, User_contacts
+from similarity.jarowinkler import JaroWinkler
+
+def get_contact_info(db,User_contacts,user_base_id, person_name):
+    userId_contacts = list(db.query(User_contacts).filter_by(user_base_id=user_base_id))
+
+    if len(userId_contacts) == 0:
+        return [-1,-1,-1,-1]
+
+    userId_contacts_name_email_num = [[contact_fname + " " +contact_lname, e.contact_email,e.contact_mobile_number] for e in userId_contacts]
+
+    name_similarity = [[0,0,0,0]]*len(userId_contacts_name_email)
+
+    for i,el in enumerate(userId_contacts_name_email):
+        name_similarity[i] = [jarowinkler.similarity(person_name, el[0]),el[0],el[1],el[2]]
+
+    best_matched_contact = sorted(name_similarity, key=lambda x: x[0])[-1]
+
+    if best_matched_contact[0] >= 0.8:
+        return best_matched_contact
+    else:
+        return [0,0,0,0]
